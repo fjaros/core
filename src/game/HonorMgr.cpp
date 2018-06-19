1698d6c5f67ed3747a4b3cfcb29766019c16e77b
brotalnia <brotalnia@gmail.com>
2018-06-19 19:40:38 +0300

    Remove ticket message and auto restart.

--- src/game/HonorMgr.cpp
+++ src/game/HonorMgr.cpp
@@ -525,7 +525,8 @@ void HonorMaintenancer::CheckMaintenanceDay()
     if (sWorld.GetGameDay() >= m_nextMaintenanceDay && !m_markerToStart)
     {
         // Restart 15 minutes after honor weekend by server time
-        sWorld.ShutdownServ(900, SHUTDOWN_MASK_RESTART, SHUTDOWN_EXIT_CODE);
+        //sWorld.ShutdownServ(900, SHUTDOWN_MASK_RESTART, SHUTDOWN_EXIT_CODE);
+        sLog.outInfo("HonorMaintenancer: Server needs to be restarted for honor rank calculations.");
         ToggleMaintenanceMarker();
     }
 }
