da3cd382e3929fbe6c3cc4f133fce271c11dc641
Filip Jaros <fjaros91@gmail.com>
2018-09-01 11:44:03 -0700

    new deadmines loot

---
+++ sql/TheConstruct/deadmines_loot.sql
@@ -0,0 +1,285 @@
+
+-- create world drop enchant template
+INSERT INTO  `reference_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, `patch_min`, `patch_max`) VALUES
+('36001', '11207', '0', '1', '1', '1', '0', '0', '10'),
+('36001', '16214', '0', '1', '1', '1', '0', '0', '10'),
+('36001', '16215', '0', '1', '1', '1', '0', '0', '10'),
+('36001', '16216', '0', '1', '1', '1', '0', '0', '10'),
+('36001', '16217', '0', '1', '1', '1', '0', '0', '10'),
+('36001', '16218', '0', '1', '1', '1', '0', '0', '10'),
+('36001', '16219', '0', '1', '1', '1', '0', '0', '10'),
+('36001', '16220', '0', '1', '1', '1', '0', '0', '10'),
+('36001', '16221', '0', '1', '1', '1', '0', '0', '10'),
+('36001', '16222', '0', '1', '1', '1', '0', '0', '10'),
+('36001', '16223', '0', '1', '1', '1', '0', '0', '10'),
+('36001', '16224', '0', '1', '1', '1', '0', '0', '10'),
+('36001', '16242', '0', '1', '1', '1', '0', '0', '10'),
+('36001', '16244', '0', '1', '1', '1', '0', '0', '10'),
+('36001', '16245', '0', '1', '1', '1', '0', '0', '10'),
+('36001', '16246', '0', '1', '1', '1', '0', '0', '10'),
+('36001', '16247', '0', '1', '1', '1', '0', '0', '10'),
+('36001', '16248', '0', '1', '1', '1', '0', '0', '10'),
+('36001', '16249', '0', '1', '1', '1', '0', '0', '10'),
+('36001', '16250', '0', '1', '1', '1', '0', '0', '10'),
+('36001', '16251', '0', '1', '1', '1', '0', '0', '10'),
+('36001', '16252', '0', '1', '1', '1', '0', '0', '10'),
+('36001', '16253', '0', '1', '1', '1', '0', '0', '10'),
+('36001', '16254', '0', '1', '1', '1', '0', '0', '10'),
+('36001', '16255', '0', '1', '1', '1', '0', '0', '10'),
+('36001', '19444', '0', '1', '1', '1', '0', '0', '10'),
+('36001', '19448', '0', '1', '1', '1', '0', '0', '10');
+
+-- create world drop boe template
+INSERT INTO  `reference_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, `patch_min`, `patch_max`) VALUES
+('36002', '3075', '0', '1', '1', '1', '0', '0', '10'),
+('36002', '14549', '0', '1', '1', '1', '0', '0', '10'),
+('36002', '942', '0', '1', '1', '1', '0', '0', '10'),
+('36002', '871', '0', '1', '1', '1', '0', '0', '10'),
+('36002', '2164', '0', '1', '1', '1', '0', '0', '10'),
+('36002', '943', '0', '1', '1', '1', '0', '0', '10'),
+('36002', '14552', '0', '1', '1', '1', '0', '0', '10'),
+('36002', '3475', '0', '1', '1', '1', '0', '0', '10'),
+('36002', '14553', '0', '1', '1', '1', '0', '0', '10'),
+('36002', '14554', '0', '1', '1', '1', '0', '0', '10'),
+('36002', '2246', '0', '1', '1', '1', '0', '0', '10'),
+('36002', '20698', '0', '1', '1', '1', '0', '0', '10'),
+('36002', '1728', '0', '1', '1', '1', '0', '0', '10'),
+('36002', '14555', '0', '1', '1', '1', '0', '0', '10'),
+('36002', '2244', '0', '1', '1', '1', '0', '0', '10');
+
+-- create normie mount drop template
+INSERT INTO  `reference_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, `patch_min`, `patch_max`) VALUES
+('36003', '13335', '0', '1', '1', '1', '0', '0', '10'),
+('36003', '23720', '0', '1', '1', '1', '0', '0', '10'),
+('36003', '13317', '0', '1', '1', '1', '0', '0', '10'),
+('36003', '8586', '0', '1', '1', '1', '0', '0', '10'),
+('36003', '12351', '0', '1', '1', '1', '0', '0', '10'),
+('36003', '12330', '0', '1', '1', '1', '0', '0', '10'),
+('36003', '15292', '0', '1', '1', '1', '0', '0', '10'),
+('36003', '15293', '0', '1', '1', '1', '0', '0', '10'),
+('36003', '18245', '0', '1', '1', '1', '0', '0', '10'),
+('36003', '18247', '0', '1', '1', '1', '0', '0', '10'),
+('36003', '18246', '0', '1', '1', '1', '0', '0', '10'),
+('36003', '18248', '0', '1', '1', '1', '0', '0', '10');
+
+-- create rare mount drop template
+INSERT INTO  `reference_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, `patch_min`, `patch_max`) VALUES
+('36004', '19030', '0', '1', '1', '1', '0', '0', '10'),
+('36004', '12302', '0', '1', '1', '1', '0', '0', '10'),
+('36004', '12303', '0', '1', '1', '1', '0', '0', '10'),
+('36004', '13327', '0', '1', '1', '1', '0', '0', '10'),
+('36004', '13326', '0', '1', '1', '1', '0', '0', '10'),
+('36004', '13328', '0', '1', '1', '1', '0', '0', '10'),
+('36004', '13329', '0', '1', '1', '1', '0', '0', '10'),
+('36004', '12354', '0', '1', '1', '1', '0', '0', '10'),
+('36004', '12353', '0', '1', '1', '1', '0', '0', '10'),
+('36004', '18768', '0', '1', '1', '1', '0', '0', '10'),
+('36004', '12327', '0', '1', '1', '1', '0', '0', '10'),
+('36004', '12325', '0', '1', '1', '1', '0', '0', '10'),
+('36004', '12326', '0', '1', '1', '1', '0', '0', '10');
+
+/* mob loot templates */
+
+-- defias miner - 598
+DELETE FROM `creature_loot_template` WHERE `entry` = '598';
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, `patch_min`, `patch_max`) VALUES
+('598', '36002', '0.5', '0', '-36002', '1', '0', '0', '10'),
+('598', '14156', '2', '0', '1', '1', '0', '0', '10');
+
+-- defias strip miner - 4416
+DELETE FROM `creature_loot_template` WHERE `entry` = '4416';
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, `patch_min`, `patch_max`) VALUES
+('4416', '14156', '2', '0', '1', '1', '0', '0', '10'),
+('4416', '36002', '0.5', '0', '-36002', '1', '0', '0', '10');
+
+-- defias evoker - 1729
+DELETE FROM `creature_loot_template` WHERE `entry` = '1729';
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, `patch_min`, `patch_max`) VALUES
+('1729', '14156', '2', '0', '1', '1', '0', '0', '10'),
+('1729', '36001', '10', '0', '-36001', '1', '0', '0', '10'),
+('1729', '36002', '0.5', '0', '-36002', '1', '0', '0', '10');
+
+-- defias wizard - 4418
+DELETE FROM `creature_loot_template` WHERE `entry` = '4418';
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, `patch_min`, `patch_max`) VALUES
+('4418', '14156', '2', '0', '1', '1', '0', '0', '10'),
+('4418', '36001', '10', '0', '-36001', '1', '0', '0', '10'),
+('4418', '36002', '0.5', '0', '-36002', '1', '0', '0', '10');
+
+-- defias overseer - 634
+DELETE FROM `creature_loot_template` WHERE `entry` = '634';
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, `patch_min`, `patch_max`) VALUES
+('634', '14156', '2', '0', '1', '1', '0', '0', '10'),
+('634', '36002', '0.5', '0', '-36002', '1', '0', '0', '10');
+
+-- defias taskmaster - 4417
+DELETE FROM `creature_loot_template` WHERE `entry` = '4417';
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, `patch_min`, `patch_max`) VALUES
+('4417', '14156', '2', '0', '1', '1', '0', '0', '10'),
+('4417', '36002', '0.5', '0', '-36002', '1', '0', '0', '10');
+
+-- defias watchman - 1725
+DELETE FROM `creature_loot_template` WHERE `entry` = '1725';
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, `patch_min`, `patch_max`) VALUES
+('1725', '14156', '2', '0', '1', '1', '0', '0', '10'),
+('1725', '36002', '0.5', '0', '-36002', '1', '0', '0', '10');
+
+-- defias blackguard - 636
+DELETE FROM `creature_loot_template` WHERE `entry` = '636';
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, `patch_min`, `patch_max`) VALUES
+('636', '14156', '2', '0', '1', '1', '0', '0', '10'),
+('636', '36002', '0.5', '0', '-36002', '1', '0', '0', '10');
+
+-- defias pirate - 657
+DELETE FROM `creature_loot_template` WHERE `entry` = '657';
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, `patch_min`, `patch_max`) VALUES
+('657', '14156', '2', '0', '1', '1', '0', '0', '10'),
+('657', '36002', '0.5', '0', '-36002', '1', '0', '0', '10');
+
+-- defias squallshaper - 1732
+DELETE FROM `creature_loot_template` WHERE `entry` = '1732';
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, `patch_min`, `patch_max`) VALUES
+('1732', '14156', '2', '0', '1', '1', '0', '0', '10'),
+('1732', '36002', '0.5', '0', '-36002', '1', '0', '0', '10');
+
+-- goblin woodcarver - 641
+DELETE FROM `creature_loot_template` WHERE `entry` = '641';
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, `patch_min`, `patch_max`) VALUES
+('641', '14156', '2', '0', '1', '1', '0', '0', '10'),
+('641', '36002', '0.5', '0', '-36002', '1', '0', '0', '10');
+
+-- goblin engineer - 622
+DELETE FROM `creature_loot_template` WHERE `entry` = '622';
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, `patch_min`, `patch_max`) VALUES
+('622', '14156', '2', '0', '1', '1', '0', '0', '10'),
+('622', '18653', '10', '0', '1', '1', '0', '0', '10'),
+('622', '36002', '0.5', '0', '-36002', '1', '0', '0', '10');
+
+-- goblin craftsman - 1731
+DELETE FROM `creature_loot_template` WHERE `entry` = '1731';
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, `patch_min`, `patch_max`) VALUES
+('1731', '14156', '2', '0', '1', '1', '0', '0', '10'),
+('1731', '36002', '0.5', '0', '-36002', '1', '0', '0', '10');
+
+-- goblin shipbuilder - 3947
+DELETE FROM `creature_loot_template` WHERE `entry` = '3947';
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, `patch_min`, `patch_max`) VALUES
+('3947', '14156', '2', '0', '1', '1', '0', '0', '10'),
+('3947', '36002', '0.5', '0', '-36002', '1', '0', '0', '10');
+
+/* boss loot templates */
+
+-- rhahkzor - 644
+DELETE FROM `creature_loot_template` WHERE `entry` = '644';
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, `patch_min`, `patch_max`) VALUES
+('644', '36003', '3', '0', '-36003', '1', '0', '0', '10'),
+('644', '36004', '0.25', '0', '-36004', '1', '0', '0', '10');
+
+-- miner johnson - 3586
+DELETE FROM `creature_loot_template` WHERE `entry` = '3586';
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, `patch_min`, `patch_max`) VALUES
+('3586', '36003', '3', '0', '-36003', '1', '0', '0', '10'),
+('3586', '36004', '0.25', '0', '-36004', '1', '0', '0', '10');
+
+-- sneeds shredder - 642
+DELETE FROM `creature_loot_template` WHERE `entry` = '642';
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, `patch_min`, `patch_max`) VALUES
+('642', '36003', '3', '0', '-36003', '1', '0', '0', '10'),
+('642', '36004', '0.25', '0', '-36004', '1', '0', '0', '10');
+
+-- sneed - 643
+DELETE FROM `creature_loot_template` WHERE `entry` = '643';
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, `patch_min`, `patch_max`) VALUES
+('643', '36003', '3', '0', '-36003', '1', '0', '0', '10'),
+('643', '36004', '0.25', '0', '-36004', '1', '0', '0', '10');
+
+-- gilnid - 1763
+DELETE FROM `creature_loot_template` WHERE `entry` = '1763';
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, `patch_min`, `patch_max`) VALUES
+('1763', '36003', '3', '0', '-36003', '1', '0', '0', '10'),
+('1763', '36004', '0.25', '0', '-36004', '1', '0', '0', '10');
+
+-- mr smite - 646
+DELETE FROM `creature_loot_template` WHERE `entry` = '646';
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, `patch_min`, `patch_max`) VALUES
+('646', '36003', '3', '0', '-36003', '1', '0', '0', '10'),
+('646', '36004', '0.25', '0', '-36004', '1', '0', '0', '10');
+
+-- captain greenskin - 647
+DELETE FROM `creature_loot_template` WHERE `entry` = '647';
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, `patch_min`, `patch_max`) VALUES
+('647', '36003', '3', '0', '-36003', '1', '0', '0', '10'),
+('647', '36004', '0.25', '0', '-36004', '1', '0', '0', '10');
+
+-- vancleef - 639
+DELETE FROM `creature_loot_template` WHERE `entry` = '639';
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, `patch_min`, `patch_max`) VALUES
+('639', '36003', '3', '0', '-36003', '1', '0', '0', '10'),
+('639', '36004', '0.25', '0', '-36004', '1', '0', '0', '10');
+
+-- cookie - 645
+DELETE FROM `creature_loot_template` WHERE `entry` = '645';
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, `patch_min`, `patch_max`) VALUES
+('645', '36003', '3', '0', '-36003', '1', '0', '0', '10'),
+('645', '36004', '0.25', '0', '-36004', '1', '0', '0', '10');
+
+-- zg shoulder signets
+INSERT INTO  `reference_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, `patch_min`, `patch_max`) VALUES
+('36005', '20076', '0', '1', '1', '1', '0', '0', '10'),
+('36005', '20077', '0', '1', '1', '1', '0', '0', '10');
+
+-- zg class enchants
+INSERT INTO  `reference_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, `patch_min`, `patch_max`) VALUES
+('36006', '19790', '0', '1', '1', '1', '89', '0', '10'),
+('36006', '19784', '0', '1', '1', '1', '100', '0', '10'),
+('36006', '19785', '0', '1', '1', '1', '96', '0', '10'),
+('36006', '19788', '0', '1', '1', '1', '67', '0', '10'),
+('36006', '19782', '0', '1', '1', '1', '98', '0', '10'),
+('36006', '19787', '0', '1', '1', '1', '90', '0', '10'),
+('36006', '19789', '0', '1', '1', '1', '94', '0', '10'),
+('36006', '19786', '0', '1', '1', '1', '92', '0', '10');
+
+-- rhahkzor - 644
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, `patch_min`, `patch_max`) VALUES
+('644', '36005', '1', '0', '-36005', '1', '0', '0', '10'),
+('644', '36006', '1', '0', '-36006', '1', '0', '0', '10');
+
+-- miner johnson - 3586
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, `patch_min`, `patch_max`) VALUES
+('3586', '36005', '1', '0', '-36005', '1', '0', '0', '10'),
+('3586', '36006', '1', '0', '-36006', '1', '0', '0', '10');
+
+-- sneeds shredder - 642
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, `patch_min`, `patch_max`) VALUES
+('642', '36005', '1', '0', '-36005', '1', '0', '0', '10'),
+('642', '36006', '1', '0', '-36006', '1', '0', '0', '10');
+
+-- sneed - 643
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, `patch_min`, `patch_max`) VALUES
+('643', '36005', '1', '0', '-36005', '1', '0', '0', '10'),
+('643', '36006', '1', '0', '-36006', '1', '0', '0', '10');
+
+-- gilnid - 1763
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, `patch_min`, `patch_max`) VALUES
+('1763', '36005', '1', '0', '-36005', '1', '0', '0', '10'),
+('1763', '36006', '1', '0', '-36006', '1', '0', '0', '10');
+
+-- mr smite - 646
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, `patch_min`, `patch_max`) VALUES
+('646', '36005', '1', '0', '-36005', '1', '0', '0', '10'),
+('646', '36006', '1', '0', '-36006', '1', '0', '0', '10');
+
+-- captain greenskin - 647
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, `patch_min`, `patch_max`) VALUES
+('647', '36005', '1', '0', '-36005', '1', '0', '0', '10'),
+('647', '36006', '1', '0', '-36006', '1', '0', '0', '10');
+
+-- vancleef - 639
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, `patch_min`, `patch_max`) VALUES
+('639', '36005', '1', '0', '-36005', '1', '0', '0', '10'),
+('639', '36006', '1', '0', '-36006', '1', '0', '0', '10');
+
+-- cookie - 645
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, `patch_min`, `patch_max`) VALUES
+('645', '36005', '1', '0', '-36005', '1', '0', '0', '10'),
+('645', '36006', '1', '0', '-36006', '1', '0', '0', '10');
